0f778a840ce88f5698a9305999fa94e79e82111b
hangaebal <hangaebal@gmail.com>
2016-05-03 23:26:49 +0900

    init commit

---
+++ src/main/java/com/hangaebal/BookmarkRestApplication.java
@@ -0,0 +1,12 @@
+package com.hangaebal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkRestApplication.class, args);
+	}
+}
